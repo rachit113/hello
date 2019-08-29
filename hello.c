b280787a6b69df085312a70445c7879f8d6fb1f1
Rachit Rajiv <rachit113@gmail.com>
2019-08-29 18:34:35 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Zijie Dong  Rachit Rajiv 
+ * Date: 2019/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Zijie Dong Rachit Rajiv \n");
+
+  return 0;
+}
