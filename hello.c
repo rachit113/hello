01dd4b8882f4124e0dd0594d2edd5baacc4d61ef
zdong6 <54691829+zdong6@users.noreply.github.com>
2019-08-29 19:21:04 -0500

    Update hello.c
    I added my name and major

--- hello.c
+++ hello.c
@@ -12,6 +12,8 @@ int main(int argc, char **argv) {
 
   printf("Rachit Rajiv \n");
   printf("Computer Engineering\n");
+  printf("Zijie Dong \n");
+  printf("Computer Science \n");
 
   return 0;
 }
